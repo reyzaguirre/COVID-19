4bc6bfece5e7977885256040e6c279e5030ceead
Raul Eyzaguirre <reyzaguirre@gmail.com>
2021-04-21 10:25:34 -0500

    update

--- RCode/varios.R
+++ RCode/varios.R
@@ -418,16 +418,21 @@ library(ggplot2)
 library(reshape2)
 library(st4gi)
 
-laboratorios <- c("Sinopharm", "Pfizer")
+laboratorios <- c("Sinopharm", "Pfizer", "AstraZeneca")
 nl <- length(laboratorios)
 
 d <- data.frame(Fecha = c(rep(Sys.Date(), nl), rep("2021-02-06", nl),
                           "2021-02-07", "2021-02-13", "2021-03-03", "2021-03-10",
-                          "2021-03-17", "2021-03-24", "2021-03-31"),
-                Cantidad = c(rep(0, nl * 2), .3, .7, .05, .167, .05, .05, .05),
-                Laboratorio = c(laboratorios, laboratorios, "Sinopharm",
-                                "Sinopharm", "Pfizer", "Pfizer", "Pfizer",
-                                "Pfizer", "Pfizer"))
+                          "2021-03-17", "2021-03-24", "2021-03-31", "2021-04-07",
+                          "2021-04-14", "2021-04-18"),
+                Cantidad = c(rep(0, nl * 2),
+                             .3, .7, .05, .167,
+                             .05, .05, .0414, .2,
+                             .2007, .276),
+                Laboratorio = c(rep(laboratorios, 2),
+                                "Sinopharm", "Sinopharm", "Pfizer", "Pfizer",
+                                "Pfizer", "Pfizer", "Pfizer", "Pfizer",
+                                "Pfizer", "AstraZeneca"))
 
 d$Fecha <- as.Date(d$Fecha, "%Y-%m-%d")
 
